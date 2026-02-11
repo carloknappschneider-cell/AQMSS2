4dd0bc298271d9d6e43c8cd7b606ab2f078457d5
carloknappschneider-cell <carloknappschneider@gmail.com>
2026-02-11 15:49:11 +0100

    Add files via upload

---
+++ assignment1/gapminder.R
@@ -0,0 +1,19 @@
+install.packages("gapminder")
+library(gapminder)
+read.csv("C:/Users/carlo/Downloads/gapminder.csv")
+
+library(ggplot2)
+gapminder <- read.csv("C:/Users/carlo/Downloads/gapminder.csv")
+
+head(gapminder)
+str(gapminder)
+
+countries <- c("Spain", "France", "Germany", "United Kingdom")
+df <- gapminder[gapminder$country %in% countries, ]
+ggplot(df, aes(x = year, y = lifeExp, color = country)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Year", y = "Life expectancy",
+       title = "Life expectancy over time") +
+  theme_minimal()
+ggsave("assignment1/ass1_plot.png", width = 7, height = 5)
